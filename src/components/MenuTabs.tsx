25f2a0c25d177aa22c447cc4e5764186e0da11c4
Houdini <tomkiboi@yahoo.com>
2025-01-25 12:04:42 +0300

    Update MenuTabs.tsx Updated menu tabs for trending coins

--- src/components/MenuTabs.tsx
+++ src/components/MenuTabs.tsx
@@ -5,11 +5,11 @@ export default function MenuTabs() {
   return (
     <Tab.List className="flex flex-wrap items-center w-full px-3 py-2 mx-auto bg-white border rounded-md shadow-sm sm:items-stretch sm:flex-row sm:max-w-md justify-evenly sm:gap-2 border-slate-200">
       <Tab className="py-2 font-medium transition-colors border-b-2 border-white px-1.5 ui-selected:outline-none ui-selected:text-emerald-500 ui-selected:border-b-emerald-500">
-        Trending
+        Trending Crypto
       </Tab>
 
       <Tab className="py-2 font-medium transition-colors border-b-2 border-white px-1.5 ui-selected:outline-none ui-selected:text-emerald-500 ui-selected:border-b-emerald-500">
-        Cryptocurrencies
+        Crypto Currencies
       </Tab>
 
       <Tab className="py-2 font-medium transition-colors border-b-2 border-white px-1.5 ui-selected:outline-none ui-selected:text-emerald-500 ui-selected:border-b-emerald-500">
