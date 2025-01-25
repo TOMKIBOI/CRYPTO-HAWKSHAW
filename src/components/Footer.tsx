1cfb00c69b927ab95c14c550338fb6237c1dcbc5
Houdini <tomkiboi@yahoo.com>
2025-01-25 12:00:28 +0300

    Update Footer.tsx footer change

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,13 +2,13 @@ export default function Footer() {
   return (
     <footer className="py-4 mt-auto text-sm text-center border-t text-slate-400 border-slate-200">
       <p className="mx-2">
-        CryptoTracker © {new Date().getFullYear()}{' '}
+        Crypto Hawkshaw © {new Date().getFullYear()}{' '}
         <a
-          href="https://github.com/feliperdamaceno"
+          href="https://github.com/TOMKIBOI"
           target="_blank"
           rel="author"
         >
-          feliperdamaceno
+          TOM KIBOI
         </a>
       </p>
     </footer>
